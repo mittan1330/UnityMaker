b892ab71cb55f5622870533a8bbef4e557e176d9
mittan1330 <hudabo1330@gmail.com>
2022-09-29 00:49:13 +0900

    Baseクラスの追加

---
+++ Assets/Scripts/Manager/Enums.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel;
+
+namespace Game.Core
+{
+    /// <summary>
+    /// それぞれのInGame部分でコアになるゲームオブジェクトの名前を定義
+    /// </summary>
+    public enum CoreGameObject
+    {
+        InGameStageManager,
+        InGameLoadingManager,
+    }
+
+    public enum StageGroupes
+    {
+        //A1.GetName()を利用することでstring型の"A-1"を取得できそう？
+        [Description("A-1")]
+        A1,
+        A2,
+        A3,
+        B1,
+        B2,
+        B3,
+    }
+}
---
+++ Assets/Scripts/Manager/PointManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Game.Core
+{
+    /// <summary>
+    /// ポイント保存に関するクラス
+    /// </summary>
+    public class PointManager : MonoBehaviour
+    {
+        public static int LoadData(string dataId)
+        {
+            return PlayerPrefs.GetInt(dataId, 0);
+        }
+
+        private static void SaveData(string dataId, int value)
+        {
+            PlayerPrefs.SetInt(dataId, value);
+        }
+    }
+}
